959d417b3c177163a96f0cc5a6aa6e2cd3a8caab
minju1009 <minju1009@gmail.com>
2021-10-20 20:04:24 +0900

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const calculator = {
+    add:function(a,b){
+        console.log(a+b);
+    },
+    minus:function(a,b){
+        console.log(a-b);
+    },
+    powerof:function(a,b){
+        console.log(a**b);
+    },
+    divide:function(a,b){
+        console.log(a/b);
+    }
+}
+
+calculator.add(10,5);
+calculator.minus(10,5);
+calculator.powerof(10,5);
+calculator.divide(10,5);
+
