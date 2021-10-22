252a2eb035f0c567787c3a2ff7f664ff7883b8e5
minju1009 <minju1009@gmail.com>
2021-10-22 23:55:30 +0900

    Update javascript_html.js

--- javascript_html.js
+++ javascript_html.js
@@ -1,33 +1,10 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector(".hello:first-child h1");
 
-console.dir(title);
 
 function handleTitleClick() {
-    title.style.color = "blue";
+    h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowOffline () {
-    alert("SOS no WIFI");
-}
-
-
-title.addEventListener("click",  handleMouseEnter);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindoWResize);
-window.addEventListener("offline", handleWindowOffline);
+h1.addEventListener("click", handleTitleClick);
 
 
