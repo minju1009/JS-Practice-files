7123c4681e4e9a1ca9259911f1643d17127b9206
minju1009 <minju1009@gmail.com>
2021-10-24 23:53:54 +0900

    Update javascript_html.js

--- javascript_html.js
+++ javascript_html.js
@@ -8,3 +8,15 @@ function handleTitleClick() {
 h1.addEventListener("click", handleTitleClick);
 
 
+
+
+const background = document.querySelector("body");
+const colors = ["#2E8CD5", "#2E8CD5", "#914EAD"];
+
+function resize(){
+  const currentSize = window.innerWidth;
+  if(currentSize<=400){
+    background.style.backgroundColor = colors[1];
+  } else 
+}
+
