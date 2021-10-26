4b56b8dbb6e42d50e620849dc7b41a7c636ef409
minju1009 <minju1009@gmail.com>
2021-10-27 00:36:08 +0900

    Update randomgame.js

--- randomgame.js
+++ randomgame.js
@@ -4,45 +4,34 @@ const userchoice = document.querySelector("#user-guessing-number");
 const choiceResult = document.querySelector("#choice-result");
 const resultReport = document.querySelector("#result-report");
 
+function gameStart(event) {
+  event.preventDefault();
+  const userRangeNumber = userInputNumber.value;
+  const userChoiceNumber = userchoice.value;
 
-
-
-function gameStart(USERINPUTNUMBER, userChoiceNumber) {
-    userInputNumber.preventDefault();
-    userChoiceNumber.preventDefault();
-    const userRangeNumber = userInputNumber.value;
-    const userChoiceNumber = userchoice.value;
-    
-    if(userRangeNumber < 0) {
-        alert("Please write a positive number");
-    }else if (userChoiceNumber === ""){
-        alert("Please guess a number.")
-    } else if (userChoiceNumber > userRangeNumber){
-        alert("Please type in the number between 0 and " + userRangeNumber)
+  if (userRangeNumber < 0) {
+    alert("Please write a positive number");
+  } else if (userChoiceNumber === "") {
+    alert("Please guess a number.");
+  } else if (userChoiceNumber > userRangeNumber) {
+    alert("Please type in the number between 0 and " + userRangeNumber);
+  } else {
+    const computerRandomNumber = Math.floor(Math.random() * userRangeNumber);
+    if (userChoiceNumber < computerRandomNumber) {
+      numberResult(userChoiceNumber, computerRandomNumber);
+      resultReport.innerText = "You lost!";
+      resultReport.classList.remove("hidden");
     } else {
-        const computerRandomNumber = Math.floor(Math.random()*userRangeNumber);
-        if (userChoiceNumber<computerRandomNumber){
-            numberResult (userChoiceNumber, computerRandomNumber);
-            declareResult (lost);
-        }
-        else {
-            numberResult (userChoiceNumber, computerRandomNumber);
-            declareResult (won);
-        }
-        
-    }        
+      numberResult(userChoiceNumber, computerRandomNumber);
+      resultReport.innerText = "You Won!";
+      resultReport.classList.remove("hidden");
+    }
+  }
 }
 
-
-function numberResult (userChoiceNumber, computerRandomNumber){
-    choiceResult.innerText= `You chose: ${userChoiceNumber}, The machine chose: ${computerRandomNumber}`;
-    choiceResult.classList.remove("hidden");
+function numberResult(userChoiceNumber, computerRandomNumber) {
+  choiceResult.innerText = `You chose: ${userChoiceNumber}, The machine chose: ${computerRandomNumber}.`;
+  choiceResult.classList.remove("hidden");
 }
 
-function declareResult (result) {
-    resultReport.innerText = `You ${result}!`;
-    resultReport.classList.remove("hidden");
-}
-
-
-playbutton.addEventListener("submit",gameStart);
+playbutton.addEventListener("submit", gameStart);
