a09882528a90a927e7bc69718b79ceeaa3323a42
minju1009 <minju1009@gmail.com>
2021-10-29 16:45:38 +0900

    Create calculator.js

---
+++ calculator/calculator.js
@@ -0,0 +1,24 @@
+const calculator = {
+    plus:function(a,b){
+        return a + b;
+    },
+    minus:function(a,b){
+        return a - b;
+    },
+    powerof:function(a,b){
+        return a ** b ;
+    },
+    divide:function(a,b){
+        return a / b;
+    },
+    times:function(a,b){
+        return a * b ;
+    }
+}
+
+const plusResult = calculator.plus(2,3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult,plusResult);
+const powerResult = calculator.powerof(divideResult,minusResult);
+
